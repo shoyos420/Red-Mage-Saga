61b3933d05dadfe87051faebbe28ed945948552d
shoyos420 <shoyos420@hotmail.com>
2016-06-10 18:19:12 -0500

    modificacion para bordes en plataforma

--- plataform.py
+++ plataform.py
@@ -85,25 +85,25 @@ def main():
         "N                                                                                                   N",
         "N                                                                                                   N",
         "N                                                                                                   N",
-        "N                    FFFFFFFFFFFF                                                                   N",
+        "N                    BFFFFFFFFFFC                                                                   N",
         "N                                                                                                   N",
         "N                                                                                                   N",
         "N                                                                                                   N",
-        "N    FFFFFFFFFFFF                                                                                   N",
+        "N    BFFFFFFFFFFFC                                                                                  N",
         "N                                                                                                   N",
-        "N                          FFFFFF                                                                   N",
-        "N                 FFFFFF                                                                            N",
+        "N                          BFFFFFFC                                                                 N",
+        "N                 BFFFFFFC                                                                          N",
         "N                                                                                                   N",
-        "N         FFFFFFFF                                                                                  N",
+        "N         BFFFFFFFC                                                                                 N",
         "N                                                                                                   N",
-        "N                     FFFFFF                                                                        N",
+        "N                     BFFFFFFC                                                                      N",
         "N                                                                                                   N",
-        "N   FFFFFFFFFFFF                                                                                    N",
-        "N        A                                                   A                                       N",
-        "N                 FFFFFFFFFFFF                                                                      N",
+        "N   BFFFFFFFFFFC                                                                                    N",
+        "N        A                                                   A                                      N",
+        "N                 BFFFFFFFFFFC                                                                      N",
         "N                                                                                                   N",
         "N                                                                                                   N",
-        "N                 FFFFFFFFFFFF FFFFFFFFFFFF                                                         N",
+        "N                 BFFFFFFFFFFC BFFFFFFFFFFC                                                         N",
         "N                                                                                                   N",
         "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",]
     # build thE lEvEl",]  
@@ -129,6 +129,14 @@ def main():
                 entities.add(e)
             if col == "F":
                 e = PlatformFloor(x, y)
+                platforms.append(e)
+                entities.add(e)
+            if col == "B":
+                e = PlatformB(x, y)
+                platforms.append(e)
+                entities.add(e)
+            if col == "C":
+                e = PlatformC(x, y)
                 platforms.append(e)
                 entities.add(e)
             x += 16*2
@@ -377,14 +385,39 @@ class nones(Entity):
 class avisos(Entity):
     def __init__(self, x, y, A_count):
         Entity.__init__(self)
-        self.image = Surface((70, 50))
+        self.image = Surface((1, 1))
         self.image =  pygame.image.load("media/graphics/aviso"+str(A_count)+".png").convert_alpha()
         self.image = pygame.transform.scale(self.image,(110 ,90) )
-        self.rect = Rect(x, y, 16*2, 16*2)
+        self.rect = Rect(x, y, 1, 1)
 
     def update(self):
         pass
 
+
+class PlatformB(Entity):
+    def __init__(self, x, y):
+        Entity.__init__(self)
+        #self.image = Surface((32, 32))
+        #self.image.convert()
+        #self.image.fill(Color("#DDDDDD"))
+        self.image =  pygame.image.load("media/graphics/grassB.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image,(16*2 ,16*2) )
+        self.rect = Rect(x, y, 16*2, 16*2)
+
+    def update(self):
+        pass
+class PlatformC(Entity):
+    def __init__(self, x, y):
+        Entity.__init__(self)
+        #self.image = Surface((32, 32))
+        #self.image.convert()
+        #self.image.fill(Color("#DDDDDD"))
+        self.image =  pygame.image.load("media/graphics/grassC.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image,(16*2 ,16*2) )
+        self.rect = Rect(x, y, 16*2, 16*2)
+
+    def update(self):
+        pass
 
 class ExitBlock(Platform):
     def __init__(self, x, y):
