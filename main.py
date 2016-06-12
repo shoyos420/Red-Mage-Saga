d796309390198db63b04ea064d93dcc5b8000587
shoyos420 <shoyos420@hotmail.com>
2016-06-11 19:18:50 -0500

    integracion del menu

---
+++ main.py
@@ -0,0 +1,17 @@
+#import pygame, os
+#from pygame.locals import *
+import plataform 
+import menu
+def main():
+    #os.environ["SDL_VIDEO_CENTERED"] = "1"
+    #pygame.mixer.pre_init(44100, -16, 2, 4096)
+    ##pygame.init()
+    #pygame.mouse.set_visible(0)
+    #pygame.display.set_icon(pygame.image.load(data.filepath("bowser1.gif")))
+    #pygame.display.set_caption("Super Mario Bros (DEMO)")
+    #screen = pygame.display.set_mode((640, 480))
+    menu.main()
+
+
+if __name__ == '__main__' :
+    main()
